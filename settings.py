995ff0a17356b2a203dfed18b2765380372c6a19
emel <98974328+emelkrhn@users.noreply.github.com>
2023-09-08 22:27:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,82 +4,6 @@ MAIN_TEXTSIZE = 20
 S_TEXTSIZE = 18
 FONT = 'Arial Rounded MT Bold'
 
-'''
-# Colors
-WHITE = '#d6d6d6'
-LIGHT_GRAY = '#c2c2c2'
-DARK_GRAY = '#737575'
-
-DARK_BLUE = '#09102e'
-COOLBLUE = '#3b446b'
-LIGHT_BLUE = '#acdafa'
-YELLOW = '#deae35'
-
-LIGHT_PINK = '#fab9f6'
-PINK = '#e055b7'
-DARK_PINK = '#942978'
-
-LILAC = '#d081de'
-DARK_PURPLE = '#a815c2'
-LIGHT_PURPLE = '#bc9bc2'
-
-
-# Colors
-BACKGROUND_COLOR = LIGHT_PURPLE
-FRAME_COLOR = LIGHT_PINK
-
-TITLE_COLOR = PINK
-TEXT_COLOR = DARK_PINK
-
-BUTTON_COLOR = DARK_PURPLE
-B_HOVER_COLOR = LILAC
-B_TEXT_COLOR = WHITE
-
-ENTRY_COLOR = WHITE
-E_TEXT_COLOR = DARK_PURPLE
-E_BORDER_COLOR = DARK_PURPLE
-
-RADIOBUTTON_COLOR = DARK_PURPLE
-RB_HOVER_COLOR = LILAC
-RB_BORDER_COLOR = DARK_PINK
-RB_TEXT_COLOR = DARK_PINK
-
-S_TEXT_COLOR = DARK_PURPLE
-S_HOVER_COLOR = LIGHT_PINK
-S_BUTTON_COLOR = PINK
-S_PROGRESS_COLOR = DARK_PINK
-
-PLOT_COLOR = DARK_PURPLE
-PLOT_COLOR2 = DARK_BLUE
-'''
-
-navy = '#164B60'
-blue = '#1B6B93'
-lightblue = '#91AEC1'
-royalblue = '#172A65'
-green = '#78BC61'
-purple = '#9F63BD'
-lilac = '#9F84BD'
-
-champane_pink = '#F5E4D7'
-mistyrose = '#E5D1D0'
-
-caramel = '#CE8147'
-butterscotch = '#CE9547'
-
-skobeloffgreen = '#007C77'
-darkcyan = '#009284'
-
-
-ylnmnblue = '#465775'
-paynesgray = '#466975'
-
-celedon = '#BEE5BF'
-teagreen = '#D1F9BF'
-
-redwood = '#9D695A'
-rosetraupe = '#9D696E'
-
 
 storm = '#0c2431' 
 teal = '#194a50'
@@ -89,24 +13,6 @@ sand = '#d8a28c'
 
 cloud = '#e1dad2'
 
-gunmetal = '#272D2D'
-space_cadet = '#272D47'
-rose_quartz = '#A39BA8'
-powder_blue = '#B8C5D6'
-alice_blue = '#EDF5FC'
-
-# team germany
-saffron = '#E3B505'
-naplesyellow = '#FFE066'
-
-pennred = '#95190C'
-choclatecosmos = '#5F021F'
-bloodred = '#660000'
-
-night = '#0C120C'
-licorice = '#1C0F13'
-
-
 
 ######
 main = storm
@@ -114,9 +20,6 @@ light_main = teal
 secondary = sunburst
 light_secondary = sand
 accent = cloud
-
-
-######
 
 
 # Colors
@@ -162,47 +65,3 @@ COMBO_DD_HOVER_COLOR = light_secondary
 COMBO_DD_TEXT_COLOR = main
 
 
-
-
-'''
-# BARBIE THEME
-white = '#ffffff'
-pink0 ='#f29ad8'
-pink1 = '#ee71c3'
-pink2 = '#f658b8'
-pink3 = '#f039b1'
-pink4 = '#e305ad'
-COOLBLUE = '#3b446b'
-
-# Colors
-BACKGROUND_COLOR = pink4
-FRAME_COLOR = pink0
-
-TITLE_COLOR = white
-TEXT_COLOR = white
-
-BUTTON_COLOR = pink2
-B_HOVER_COLOR = pink1
-B_TEXT_COLOR = white
-
-ENTRY_COLOR = pink0
-E_TEXT_COLOR = pink3
-E_BORDER_COLOR = pink1
-
-RADIOBUTTON_COLOR = pink3
-RB_HOVER_COLOR = pink2
-RB_BORDER_COLOR = pink3
-RB_TEXT_COLOR = pink4
-
-S_TEXT_COLOR = pink3
-S_HOVER_COLOR = pink2
-S_BUTTON_COLOR = pink3
-S_PROGRESS_COLOR = pink4
-S_BORDER_COLOR = pink2
-
-PLOT_COLOR = pink4
-PLOT_COLOR2 = COOLBLUE
-
-PLOT_TEXT_COLOR = pink4
-PLOT_TEXT_COLOR2 = pink3
-'''
